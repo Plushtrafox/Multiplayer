6523040d1969c659ccdbbe6c17ce378cdeb6e5f1
Christopher Alejandro Hayling Guerra <christopherhayling9@gmail.com>
2025-12-05 19:57:26 -0500

    player spawn

---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayer : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField] private PhotonView playerPrefab;
+    [SerializeField] private Transform posicionSpawn;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();    
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom();
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+
+        PhotonNetwork.Instantiate(playerPrefab.name, posicionSpawn.position, posicionSpawn.rotation);
+    }
+}
