57ea2a35eca5f564bc155858e00d1a7178b33ef8
Plushtrafox <galeanojimenez1987@gmail.com>
2025-12-15 13:54:47 -0500

    cambio

--- Assets/Scripts/Connect/ConnectServer.cs
+++ Assets/Scripts/Connect/ConnectServer.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 
 
@@ -13,6 +14,20 @@ public class ConnectServer : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         SceneManager.LoadScene(1);
+        StartCoroutine(PingLoop());
+
+    }
+    private IEnumerator PingLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            if (PhotonNetwork.IsConnected)
+            {
+                int ping = PhotonNetwork.GetPing();
+                Debug.Log("Ping: " + PhotonNetwork.GetPing() + " ms");
+            }
+        }
     }
 
 
